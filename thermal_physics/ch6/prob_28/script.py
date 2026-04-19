bc54362572ed2e18451fead9c0a96264519b384a
Gabe Decker <42gdeckers@gmail.com>
2026-04-19 11:06:38 -0600

    Finished the script to find the heat capacity when adding a certain number of terms into Z.

--- thermal_physics/ch6/prob_28/script.py
+++ thermal_physics/ch6/prob_28/script.py
@@ -1,6 +1,10 @@
-from sympy import symbols, summation, exp, diff, log
+import matplotlib.pyplot as plt
+import numpy as np
+from sympy import symbols, summation, exp, diff, log, simplify, lambdify
 
 upper_j = 6
+t_start = 0
+t_end = 3
 
 
 if __name__ == "__main__":
@@ -27,4 +31,22 @@ if __name__ == "__main__":
     print(C)
     print()
 
-    # TODO: get unitless variable, subs it, get lambda, plot
+    # Put in unitless variable
+    t = symbols("t")
+    c_in_t = C.subs(T, t*ep/k)
+    c_unitless = simplify(c_in_t / k)
+
+    # Get numpy function
+    t_vals = np.linspace(t_start, t_end, num=200)
+    c_over_k_lambda = lambdify(t, c_unitless, "numpy")
+    # NOTE: small t will cause /0 and overflow errors, but it's fine :)
+    c_over_k_vals = c_over_k_lambda(t_vals)
+
+    # Plot the results
+    plt.plot(t_vals, c_over_k_vals)
+    plt.title(f"Rotational Heat Capacity when j in [0,{upper_j}]")
+    plt.xlabel("Reduced Temperature (kT/epsilon)")
+    plt.ylabel("Heat Capacity (C/k)")
+    plt.savefig("reduced_temp-heat_capacity_curve.png")
+
+    plt.close()
